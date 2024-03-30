f78aee24d03b50cf65dbe6915036cacb64057edb
krzysckh <kpm@linux.pl>
2024-03-30 19:59:15 +0100

    .min.js is now actually not minimalized, colors

--- main.js
+++ main.js
@@ -7,19 +7,22 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-function addopt(t, el) {
+function addopt(t, el, c) {
   const cb = document.createElement("input");
   const l = document.createElement("label");
   const br = document.createElement("br");
+  const color = c || 'navyblue';
 
   cb.id = t;
   cb.name = t;
   cb.type = "checkbox";
 
   const updatething = thing => {
-    if (cb.checked)
+    if (cb.checked) {
       thing.addTo(map);
-    else
+      if (thing.setStyle)
+        thing.setStyle({color: color});
+    } else
       thing.remove();
   };
 
@@ -39,63 +42,62 @@ function addopt(t, el) {
 }
 
 const kmls = [
-  "Program-zanocuj-w-lesie.kml",
-  "szlaki/dyrekcja.kml",
-  "szlaki/granice-BdPN.kml",
-  "szlaki/granice-Natura.kml",
-  "szlaki/granice-Otulina.kml",
-  "szlaki/granice-czynna.kml",
-  "szlaki/granice-scisla.kml",
-  "szlaki/parkingi.kml",
-  "szlaki/pola.kml",
-  "szlaki/punkty.kml",
-  "szlaki/sciezki-berehy_gorne-przebieg.kml",
-  "szlaki/sciezki-berehy_gorne-przystanki.kml",
-  "szlaki/sciezki-bukowe_berdo-przebieg.kml",
-  "szlaki/sciezki-bukowe_berdo-przystanki.kml",
-  "szlaki/sciezki-gorny_san-przebieg.kml",
-  "szlaki/sciezki-gorny_san-przystanki.kml",
-  "szlaki/sciezki-polonina_carynska-przebieg.kml",
-  "szlaki/sciezki-polonina_carynska-przystanki.kml",
-  "szlaki/sciezki-polonina_wetlinska-przebieg.kml",
-  "szlaki/sciezki-polonina_wetlinska-przystanki.kml",
-  "szlaki/sciezki-rozsypaniec-krzemien-przebieg.kml",
-  "szlaki/sciezki-rozsypaniec-krzemien-przystanki.kml",
-  "szlaki/sciezki-suche_rzeki-smerek-przebieg.kml",
-  "szlaki/sciezki-suche_rzeki-smerek-przystanki.kml",
-  "szlaki/sciezki-ustrzyki_gorne-szeroki_wierch-przebieg.kml",
-  "szlaki/sciezki-ustrzyki_gorne-szeroki_wierch-przystanki.kml",
-  "szlaki/sciezki-ustrzyki_gorne-wolosate-przebieg.kml",
-  "szlaki/sciezki-ustrzyki_gorne-wolosate-przystanki.kml",
-  "szlaki/sciezki-wielka_rawka-przebieg.kml",
-  "szlaki/sciezki-wielka_rawka-przystanki.kml",
-  "szlaki/sciezki-wolosate-tarnica-przebieg.kml",
-  "szlaki/sciezki-wolosate-tarnica-przystanki.kml",
-  "szlaki/szlaki-czarny.kml",
-  "szlaki/szlaki-czerwony.kml",
-  "szlaki/szlaki-niebieski.kml",
-  "szlaki/szlaki-zielony.kml",
-  "szlaki/szlaki-zolty.kml",
-  "szlaki/wiaty-deszczochrony.kml",
-  "szlaki/wiaty-odpoczynkowe.kml",
-  "szlaki/wiaty-ogniskowe.kml",
+  { path: "Program-zanocuj-w-lesie.kml"                                 , color: 'orange' },
+  { path: "szlaki/dyrekcja.kml"                                         , color: 'black' },
+  { path: "szlaki/granice-BdPN.kml"                                     , color: 'blue' },
+  { path: "szlaki/granice-Natura.kml"                                   , color: 'green' },
+  { path: "szlaki/granice-Otulina.kml"                                  , color: 'red' },
+  { path: "szlaki/granice-czynna.kml"                                   , color: 'blue' },
+  { path: "szlaki/granice-scisla.kml"                                   , color: 'black' },
+  { path: "szlaki/parkingi.kml"                                         , color: 'black' },
+  { path: "szlaki/pola.kml"                                             , color: 'black' },
+  { path: "szlaki/punkty.kml"                                           , color: 'black' },
+  { path: "szlaki/sciezki-berehy_gorne-przebieg.kml"                    , color: 'black' },
+  { path: "szlaki/sciezki-berehy_gorne-przystanki.kml"                  , color: 'black' },
+  { path: "szlaki/sciezki-bukowe_berdo-przebieg.kml"                    , color: 'black' },
+  { path: "szlaki/sciezki-bukowe_berdo-przystanki.kml"                  , color: 'black' },
+  { path: "szlaki/sciezki-gorny_san-przebieg.kml"                       , color: 'black' },
+  { path: "szlaki/sciezki-gorny_san-przystanki.kml"                     , color: 'black' },
+  { path: "szlaki/sciezki-polonina_carynska-przebieg.kml"               , color: 'black' },
+  { path: "szlaki/sciezki-polonina_carynska-przystanki.kml"             , color: 'black' },
+  { path: "szlaki/sciezki-polonina_wetlinska-przebieg.kml"              , color: 'black' },
+  { path: "szlaki/sciezki-polonina_wetlinska-przystanki.kml"            , color: 'black' },
+  { path: "szlaki/sciezki-rozsypaniec-krzemien-przebieg.kml"            , color: 'black' },
+  { path: "szlaki/sciezki-rozsypaniec-krzemien-przystanki.kml"          , color: 'black' },
+  { path: "szlaki/sciezki-suche_rzeki-smerek-przebieg.kml"              , color: 'black' },
+  { path: "szlaki/sciezki-suche_rzeki-smerek-przystanki.kml"            , color: 'black' },
+  { path: "szlaki/sciezki-ustrzyki_gorne-szeroki_wierch-przebieg.kml"   , color: 'black' },
+  { path: "szlaki/sciezki-ustrzyki_gorne-szeroki_wierch-przystanki.kml" , color: 'black' },
+  { path: "szlaki/sciezki-ustrzyki_gorne-wolosate-przebieg.kml"         , color: 'black' },
+  { path: "szlaki/sciezki-ustrzyki_gorne-wolosate-przystanki.kml"       , color: 'black' },
+  { path: "szlaki/sciezki-wielka_rawka-przebieg.kml"                    , color: 'black' },
+  { path: "szlaki/sciezki-wielka_rawka-przystanki.kml"                  , color: 'black' },
+  { path: "szlaki/sciezki-wolosate-tarnica-przebieg.kml"                , color: 'black' },
+  { path: "szlaki/sciezki-wolosate-tarnica-przystanki.kml"              , color: 'black' },
+  { path: "szlaki/szlaki-czarny.kml"                                    , color: 'black' },
+  { path: "szlaki/szlaki-czerwony.kml"                                  , color: 'red' },
+  { path: "szlaki/szlaki-niebieski.kml"                                 , color: 'blue' },
+  { path: "szlaki/szlaki-zielony.kml"                                   , color: 'green' },
+  { path: "szlaki/szlaki-zolty.kml"                                     , color: 'yellow' },
+  { path: "szlaki/wiaty-deszczochrony.kml"                              , color: 'black' },
+  { path: "szlaki/wiaty-odpoczynkowe.kml"                               , color: 'black' },
+  { path: "szlaki/wiaty-ogniskowe.kml"                                  , color: 'black' },
 ];
 
-const szlaki = kmls.map(p => { return { path: p, val: omnivore.kml(p) }; });
-let campsdata;
-let camps;
+let szlaki = [], campsdata, camps;
 
 window.onload = async () => {
   const res = await fetch('camps.json');
   const campsjson = await res.json();
 
   campsdata = campsjson.elements.map(c => { return {pos: [c.lat || c.bounds.minlat, c.lon || c.bounds.minlon], data: c.tags} });
+
   camps = campsdata.map(e => {
     if (e.pos[0] == undefined || e.pos[1] == undefined) return;
     const marker = L.marker(e.pos);
     let text = "<table>";
     for (const [k, v] of Object.entries(e.data)) {
-      text += `<tr><td><b>${k}</b></td><td>${v}</td></tr>`;
+      text += `<tr><td><b>${k}</b></td><td>${v.replace(/[\-_]/g, " ")}</td></tr>`;
     }
     text += "</table>";
     marker.bindPopup(text);
@@ -103,7 +105,15 @@ window.onload = async () => {
   });
   addopt("Pola namiotowe", camps);
 
-  szlaki.forEach(s => {
-    addopt(s.path, s.val);
+  let ctr = 0;
+  kmls.forEach(p => {
+    const v = omnivore.kml(p.path);
+    console.log(v);
+    v.setStyle({color: 'black'});
+    szlaki.push({
+      path: p.path,
+      val: v,
+    });
+    addopt(p.path, v, p.color);
   });
 };
